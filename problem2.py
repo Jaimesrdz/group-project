65bab23d5726470f2ea881eb9d2b917745911475
Eesha Sutaria <eeshasutaria@gmail.com>
2024-10-29 23:06:37 +0000

    This is the factorial function.

--- problem2.py
+++ problem2.py
@@ -1,6 +1,7 @@
 def factorial(n):
     multiplied = n
-    while n > 0:
+    while n > 1:
         n -= 1
-        multiplied *= multiplied*n
+        multiplied *= n
     return multiplied
+print(factorial(5))
